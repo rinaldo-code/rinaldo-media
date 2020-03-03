00ba239b28ce889d7bcba2ba3ddf939dc692dc2c
rinaldo-code <61415776+rinaldo-code@users.noreply.github.com>
2020-03-03 19:48:51 +0100

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -22,10 +22,6 @@ export default class Body extends React.Component {
                   <Header {...this.props} />
                   <main id="content" className="site-content">
                     {this.props.children}
-                       <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
-    <link rel="manifest" href="/favicon/site.webmanifest">
                   </main>
                   <Footer {...this.props} />
                 </div>
