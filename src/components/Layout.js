1c4f2055e56b3d0b56800b603fe30a345bd7de23
rinaldo-code <61415776+rinaldo-code@users.noreply.github.com>
2020-03-03 20:09:30 +0100

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -17,10 +17,10 @@ export default class Body extends React.Component {
                     <meta name="google" content="notranslate" />
                     <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,700,700i" rel="stylesheet"/>
                     <link rel="stylesheet" href={safePrefix('assets/css/main.css')}/>
-                    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png"/>
-                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png"/>
-                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png"/>
-                    <link rel="manifest" href="/favicon/site.webmanifest"/>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+                    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+                    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+                    <link rel="manifest" href="/favicon/site.webmanifest" />
               </Helmet>
                 <div id="page" className={'site palette-' + _.get(this.props, 'pageContext.site.siteMetadata.color_scheme') + ' accent-' + _.get(this.props, 'pageContext.site.siteMetadata.accent_color')}>
                   <Header {...this.props} />
